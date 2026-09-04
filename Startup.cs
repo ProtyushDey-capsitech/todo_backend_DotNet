b9e63e6dfd704b771b8012cf92e3c9b414c870c5
Protyush Dey <protyush.dey@capsitech.com>
2026-09-04 16:50:35 +0530

    Fix CORS origin URL for todo-ist-ten

--- Startup.cs
+++ Startup.cs
@@ -54,7 +54,7 @@ namespace Projects
                         .WithOrigins(
                             "http://localhost:5173",
                             "https://todo-ist-git-main-protyushdey-capsitechs-projects.vercel.app",
-                        "https://todo-ist-ten.vercel.app/",
+                        "https://todo-ist-ten.vercel.app",
                         "https://todo-cs0o97v2q-protyushdey-capsitechs-projects.vercel.app/login"
                         )
                         .AllowAnyHeader()
